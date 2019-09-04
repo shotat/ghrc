03093df054d7102a2638c887ebd69047d73b71ec
Shota Terashita <shotat@users.noreply.github.com>
2019-09-04 11:27:26 +0900

    Export Spec (#6)
    * export
    
    * export branchs and labels

--- cmd/main.go
+++ cmd/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"fmt"
 
+	"bytes"
 	"github.com/shotat/ghrc"
+	"gopkg.in/yaml.v2"
 )
 
 const (
@@ -13,16 +15,31 @@ const (
 )
 
 func main() {
-	err := func() error {
-		conf, err := ghrc.LoadRepositoryConfigFromFile(filepath)
-		if err != nil {
-			return err
-		}
-
-		return conf.Apply()
-	}()
+	err := export()
 
 	if err != nil {
 		fmt.Println(err.Error())
 	}
+}
+
+func apply() error {
+	conf, err := ghrc.LoadRepositoryConfigFromFile(filepath)
+	if err != nil {
+		return err
+	}
+
+	return conf.Apply()
+}
+
+func export() error {
+	meta := &ghrc.RepositoryMetadata{"shotat", "sandbox-lol"}
+	conf, err := ghrc.ExportConfig(meta)
+	if err != nil {
+		return err
+	}
+
+	buf := bytes.NewBuffer(nil)
+	err = yaml.NewEncoder(buf).Encode(conf)
+	fmt.Println(buf.String())
+	return nil
 }
