4ecf37ee7a4b6291e6cc4f921d4f11a278fc2a00
Shota Terashita <shotat@users.noreply.github.com>
2019-09-05 00:54:00 +0900

    Add cli template (#8)
    * add cmd
    
    * add cmds

---
+++ cmd/apply.go
@@ -0,0 +1,44 @@
+package cmd
+
+import (
+	"github.com/shotat/ghrc"
+	"github.com/spf13/cobra"
+)
+
+const (
+	filepath = ".ghrc.yaml"
+)
+
+// applyCmd represents the apply command
+var applyCmd = &cobra.Command{
+	Use:   "apply",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		conf, err := ghrc.LoadRepositoryConfigFromFile(filepath)
+		if err != nil {
+			return err
+		}
+
+		return conf.Apply()
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(applyCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// applyCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// applyCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
---
+++ cmd/import.go
@@ -0,0 +1,62 @@
+package cmd
+
+import (
+	"fmt"
+
+	"bytes"
+	"github.com/shotat/ghrc"
+	"github.com/spf13/cobra"
+	"gopkg.in/yaml.v2"
+)
+
+// importCmd represents the import command
+var importCmd = &cobra.Command{
+	Use:   "import",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		owner, err := cmd.Flags().GetString("owner")
+		if err != nil {
+			return err
+		}
+		repo, err := cmd.Flags().GetString("repo")
+		if err != nil {
+			return err
+		}
+		meta := &ghrc.RepositoryMetadata{
+			Owner: owner,
+			Name:  repo,
+		}
+		conf, err := ghrc.ImportConfig(meta)
+		if err != nil {
+			return err
+		}
+
+		buf := bytes.NewBuffer(nil)
+		err = yaml.NewEncoder(buf).Encode(conf)
+		fmt.Println(buf.String())
+		return nil
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(importCmd)
+
+	importCmd.Flags().String("repo", "", "repository name")
+	importCmd.Flags().String("owner", "", "owner name")
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// importCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// importCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
